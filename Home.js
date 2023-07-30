bb05936c077a27f536292657f5ee45bfc1f5fa6d
Rayan <maedelotfi96@gmail.com>
2023-07-30 12:52:17 +0330

    done!

--- Home.js
+++ Home.js
@@ -92,12 +92,13 @@ const Home = ({navigation})=>{
       .finally(()=>{});}
 
       const Test = ()=>{
+        PushNotification.cancelAllLocalNotifications();
       PushNotification.localNotification({
         /* Android Only Properties */
         channelId: "Newchannel-daily", // (required) channelId, if the channel doesn't exist, notification will not trigger.
         ticker: "My Notification Ticker", // (optional)
-        showWhen: true, // (optional) default: true
-        autoCancel: true, // (optional) default: true
+        //showWhen: true, // (optional) default: true
+        //autoCancel: true, // (optional) default: true
         largeIcon: "ic_launcher", // (optional) default: "ic_launcher". Use "" for no large icon.
         largeIconUrl: "https://www.example.tld/picture.jpg", // (optional) default: undefined
         smallIcon: "ic_notification", // (optional) default: "ic_notification" with fallback for "ic_launcher". Use "" for default small icon.
@@ -115,7 +116,7 @@ const Home = ({navigation})=>{
         visibility: "private", // (optional) set notification visibility, default: private
         ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear). should be used in combine with `com.dieam.reactnativepushnotification.notification_foreground` setting
         shortcutId: "shortcut-id", // (optional) If this notification is duplicative of a Launcher shortcut, sets the id of the shortcut, in case the Launcher wants to hide the shortcut, default undefined
-        onlyAlertOnce: false, // (optional) alert will open only once with sound and notify, default: false
+        //onlyAlertOnce: false, // (optional) alert will open only once with sound and notify, default: false
         
         when: null, // (optional) Add a timestamp (Unix timestamp value in milliseconds) pertaining to the notification (usually the time the event occurred). For apps targeting Build.VERSION_CODES.N and above, this time is not shown anymore by default and must be opted into by using `showWhen`, default: null.
         usesChronometer: false, // (optional) Show the `when` field as a stopwatch. Instead of presenting `when` as a timestamp, the notification will show an automatically updating display of the minutes and seconds since when. Useful when showing an elapsed time (like an ongoing phone call), default: false.
@@ -134,10 +135,11 @@ const Home = ({navigation})=>{
         userInfo: {}, // (optional) default: {} (using null throws a JSON value '<null>' error)
         playSound: false, // (optional) default: true
         soundName: "default", // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
-        number: 10, // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
-        repeatType: "day", // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
+        //number: 10, // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
+        //repeatType: "day", // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
       });
-      PushNotification.removeAllDeliveredNotifications(); 
+      //PushNotification.removeAllDeliveredNotifications(); 
+      
       }
 
   useEffect(()=>{
@@ -190,7 +192,7 @@ const Home = ({navigation})=>{
 <View style={styles.ProfileView}>
 <View style={{ width:'95%', height:30,flexDirection:'row',margin:5}}>
     <View style={{height:30, width:'20%', borderColor:'black', borderWidth:1}}>
-      <Text >عنوان فعالیت</Text>
+      <Text>فعالیت</Text>
     </View>
     <View style={{height:30, width:'20%',  backgroundColor:'yellow' ,borderColor:'black', borderWidth:1}}><Text style={{textAlign:'center'}}>سطح یک</Text></View>
     <View style={{height:30, width:'20%',backgroundColor:'orange' , borderColor:'black', borderWidth:1 }}><Text style={{textAlign:'center'}}>سطح دو</Text></View>
